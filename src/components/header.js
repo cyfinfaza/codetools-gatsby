2dc8dc2e724c288dc38153278064871ba2112b59
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-04-16 01:43:37 -0400

    /about not about

--- src/components/header.js
+++ src/components/header.js
@@ -183,7 +183,7 @@ const Header = ({ pageName, hideButtons = false, hideEditorButton = false, noTab
         <a className="activefancy button-like" onClick={signOut}>
           Sign Out
         </a>
-        <a href="about" style={{ all: "unset", marginBlock: "4px", textDecoration: "underline", cursor: "pointer" }}>
+        <a href="/about" style={{ all: "unset", marginBlock: "4px", textDecoration: "underline", cursor: "pointer" }}>
           About
         </a>
       </div>
